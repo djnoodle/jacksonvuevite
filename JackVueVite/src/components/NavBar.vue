ec8f5a3b0a5698ee423d16d8129d8e6e9bfe6c7f
Jack Nyberg <nybergjack@gmail.com>
2023-02-13 17:03:18 +0100

    Made a clean slate vuevite

---
+++ JackVueVite/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<template>
+  <!-- Här lägger ni en navbar och istället för href så använder ni RouterLink -->
+  <ul>
+    <li>
+      <RouterLink to="/">Hem</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/about">About</RouterLink>
+    </li>
+  </ul>
+  <RouterView />
+</template>
